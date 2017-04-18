ae1afadd2c91ab73fdffc4b073b12efea860d7c2
Mauricio <mau.graci@gmail.com>
2017-04-17 20:17:16 -0500

    Se agregaron tiempos para la Parte a

--- P2a/src/Constants.java
+++ P2a/src/Constants.java
@@ -8,8 +8,8 @@ public interface Constants {
     int STR_SIZE = 20;      // tamaño maximo de las cadenas
     int SIN_ASIGNAR = -1;   // registro no encontrado
 
-    int MAX_CLNT = 101;
-    int MAX_SUC = 999;
+    int MAX_CLNT = 10;
+    int MAX_SUC = 10;
 
     int CLNT_DIGITS = Integer.toString(MAX_CLNT).length();
     int SUC_DIGITS = Integer.toString(MAX_SUC).length();
@@ -17,5 +17,7 @@ public interface Constants {
 
     int SAL_DECIMAL_DIGITS = 4;
     int SAL_DIGITS = 5;
+    
+    int TIME_UNIT = 1000000000; // Nano seconds
 
 }
--- P2a/src/Prueba.java
+++ P2a/src/Prueba.java
@@ -12,98 +12,79 @@ public class Prueba implements Constants {
      / mŽtodos de prueba
      /-----------------------------------------------------------------*/
     
-	private static void crear() throws Exception {
+	private static void crear() throws IOException {
         
-		try {
-            // metadatos del archivo de datos y del archivo ’ndice
-            File datos = new File( "Archivo.Datos" );
-            File denso = new File( "Indice.Denso" );
+        // metadatos del archivo de datos y del archivo ’ndice
+        File datos = new File( "Archivo.Datos" );
+        File denso = new File( "Indice.Denso" );
             
-            // handlers para manipular el contenido de los archivos
-			RandomAccessFile archivoRaF = new RandomAccessFile( datos, "rw" );
-			RandomAccessFile indiceRaF = new RandomAccessFile( denso, "rw" );
+        // handlers para manipular el contenido de los archivos
+		RandomAccessFile archivoRaF = new RandomAccessFile( datos, "rw" );
+		RandomAccessFile indiceRaF = new RandomAccessFile( denso, "rw" );
             
-            // archivo indexado usando una clave de bœsqueda de 20 bytes
-			Archivo archivo = new Archivo( archivoRaF, indiceRaF );
-			Registro registro;
-            
-            /* 
-            registro = new Registro( "Sucursal   3", 3, "Cliente 3", 300.0 );
-            archivo.insertar( registro );
-            registro = new Registro( "Sucursal   2", 2, "Cliente 2", 200.0 );
-            archivo.insertar( registro );
-            registro = new Registro( "Sucursal   0", 0, "Cliente 1", 100.0 );
-            archivo.insertar( registro );
-            registro = new Registro( "Sucursal   1", 1, "Cliente 0", 0.0 );
-            archivo.insertar( registro );
-            */
-             
-            for( int num = 1, i = 1; i <= MAX_SUC; i++ ) {
-                for( int j = 1; j <= MAX_CLNT; j++ ) {
-                    //for( int k = 1; k <= 1; k++, num++ ) {
+        // archivo indexado usando una clave de bœsqueda de 20 bytes
+		Archivo archivo = new Archivo( archivoRaF, indiceRaF );
+		Registro registro;
+                    
+        for( int num = 1, i = 1; i <= MAX_SUC; i++ ) {
+            for( int j = 1; j <= MAX_CLNT; j++ ) {
                         
-                    String suc = "Sucursal " + String.format( "%" + SUC_DIGITS +  "d", i );
-                    String nom = "Cliente " + String.format( "%" + CLNT_DIGITS + "d", j );
+                String suc = "Sucursal " + String.format( "%" + SUC_DIGITS +  "d", i );
+                String nom = "Cliente " + String.format( "%" + CLNT_DIGITS + "d", j );
                      
-                    double sal = Math.random() * (SAL_MAX - SAL_MIN) + SAL_MIN;
+                double sal = Math.random() * (SAL_MAX - SAL_MIN) + SAL_MIN;
 
-                    archivo.insertar( new Registro( suc, num++, nom, sal ) );
+                archivo.insertar( new Registro( suc, num++, nom, sal ) );
                      
-                    //}
-                }
             }
+        }
             
-            /*
-            for( int i = 0; i < 2; i ++ )
-                archivo.borrar( "Sucursal   8" );
+        /*
+        for( int i = 0; i < 2; i ++ )
+            archivo.borrar( "Sucursal   8" );
             
-            for( int i = 0; i < 2; i ++ )
-                archivo.borrar( "Sucursal   6" );
-           */
+        for( int i = 0; i < 2; i ++ )
+            archivo.borrar( "Sucursal   6" );
+        */
 
-			archivo.cerrar();
+		archivo.cerrar();
             
-		} catch( IOException e ) {
-            
-			System.out.println( "IOException:" );
-			e.printStackTrace();
-		}
 	}
     
-	private static void mostrar() {
+	private static void mostrar() throws IOException {
         
-		try {
-            // metadatos del archivo de datos y del archivo ’ndice
-            File datos = new File( "Archivo.Datos" );
-            File denso = new File( "Indice.Denso" );
+        // metadatos del archivo de datos y del archivo ’ndice
+        File datos = new File( "Archivo.Datos" );
+        File denso = new File( "Indice.Denso" );
             
-            // handlers para manipular el contenido de los archivos
-			RandomAccessFile archivoRaF = new RandomAccessFile( datos, "rw" );
-			RandomAccessFile indiceRaF = new RandomAccessFile( denso, "rw" );
+        // handlers para manipular el contenido de los archivos
+		RandomAccessFile archivoRaF = new RandomAccessFile( datos, "rw" );
+		RandomAccessFile indiceRaF = new RandomAccessFile( denso, "rw" );
             
-            // archivo indexado usando una clave de bœsqueda de 20 bytes
-			Archivo archivo = new Archivo( archivoRaF, indiceRaF );
+        // archivo indexado usando una clave de bœsqueda de 20 bytes
+		Archivo archivo = new Archivo( archivoRaF, indiceRaF );
             
-            // imprime los registros del indice y los del archivo
-            archivo.mostrar();
+        // imprime los registros del indice y los del archivo
+        archivo.mostrar();
 
-            //archivo.mostrar( "Sucursal   1" );
-            archivo.cerrar();
-            
-		} catch( IOException e ) {
-            
-			System.out.println( "IOException:" );
-			e.printStackTrace();
-		}
+        //archivo.mostrar( "Sucursal   1" );
+        archivo.cerrar();        
 	}
     
     /*-----------------------------------------------------------------
     / mŽtodo principal
     /-----------------------------------------------------------------*/
     
-	public static void main( String[] args ) throws Exception{
-        
+	public static void main( String[] args ) throws Exception {
+        double initTime = System.nanoTime();      
+
 		crear();
+
+        double endTime = System.nanoTime();
+        double time = (endTime - initTime) / TIME_UNIT;
+
 		mostrar();
+
+        System.out.println( "\nTime: " + time );
 	}
 }
